772e09a8a9a51088a3378c6c84779ae560c09c4d
羽卒 <5575298+scx69@user.noreply.gitee.com>
2021-11-19 16:25:46 +0800

    完善电子书解析

--- src/admin-imooc-node/router/book.js
+++ src/admin-imooc-node/router/book.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const multer = require('multer')
 const Result = require('../models/Result')
+const Book = require('../models/Book')
 const { UPLOAD_PATH } = require('../utils/constant')
+const boom = require('boom')
 
 const router = express.Router()
 const upload = multer({ dest: `${UPLOAD_PATH}/book` })
@@ -10,7 +12,17 @@ router.post('/upload', upload.single('file'), (req, res, next) => {
   if (!req.file || req.file.length === 0) {
     new Result('上传电子书失败').fail(res)
   } else {
-    new Result('上传电子书成功').success(res)
+    const book = new Book(req.file)
+    book
+      .parse()
+      .then(book => {
+        console.log('book', book)
+        new Result(book, '上传电子书成功').success(res)
+      })
+      .catch(err => {
+        console.log(err)
+        next(boom.badImplementation(err))
+      })
   }
 })
 
--- src/admin-imooc-node/utils/constant.js
+++ src/admin-imooc-node/utils/constant.js
@@ -2,6 +2,8 @@ const { env } = require('./env')
 const UPLOAD_PATH =
   env === 'dev' ? 'E:/upload/admin-upload-ebook' : '/root/upload/admin-upload-ebook'
 
+const UPLOAD_URL = env === 'dev' ? 'http://127.0.0.1:8089/admin-upload-ebook' : 'http://www.book.llmysnow.top/admin-upload-ebook'
+
 module.exports = {
   CODE_ERROR: -1,
   CODE_SUCCESS: 0,
@@ -10,6 +12,7 @@ module.exports = {
   PWD_SALT: 'admin_imooc_node',
   PRIVATE_KEY: 'admin_imooc_node_private_key',
   JWT_EXPIRED: 60 * 60, // token失效时间 1小时,
-  UPLOAD_PATH
+  UPLOAD_PATH,
+  MIME_TYPE_EPUB: 'application/epub',
+  UPLOAD_URL
 }
- 
