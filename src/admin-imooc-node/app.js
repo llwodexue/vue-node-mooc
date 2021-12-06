17d7da22b2a1db7f679ed934ea2a0f26767a7389
羽卒 <5575298+scx69@user.noreply.gitee.com>
2021-12-06 14:36:33 +0800

    18082

--- src/admin-imooc-node/app.js
+++ src/admin-imooc-node/app.js
@@ -13,16 +13,17 @@ app.use('/', router)
 
 const privateKey = fs.readFileSync('./https/book.llmysnow.top.key', 'utf8')
 const pem = fs.readFileSync('./https/book.llmysnow.top.pem', 'utf8')
-const credentials = {
+/* const credentials = {
   key: privateKey,
   cert: pem,
 }
-const httpsServer = https.createServer(credentials, app)
+const httpsServer = https.createServer(credentials, app) */
 
 const server = app.listen(3004, () => {
   const { port, address } = server.address()
   console.log('running on http://' + address + ':%s', port)
 })
-httpsServer.listen(18082, () => {
+/* httpsServer.listen(18082, () => {
   console.log('running on https://127.0.0.1:%s', 18082)
 })
+ */
